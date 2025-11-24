d62757a81597a7436dc4f2580e0e70b7457ff243
Jente <jvh@qbyte.be>
2025-11-24 05:05:41 +0100

    Fix nvim clipboard in docker

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,7 +6,39 @@ vim.env.PATH = vim.fn.expand("~/.local/bin") .. ":" .. vim.env.PATH
 -- General options
 opt.number = true
 opt.relativenumber = true
-opt.clipboard = "unnamedplus"
+
+-- OSC 52 clipboard support for Docker
+local function copy_to_osc52(lines)
+  local text = table.concat(lines, '\n')
+  local b64 = vim.fn.system('base64 -w0', text)
+  b64 = string.gsub(b64, '\n', '')
+  local osc52 = string.format('\027]52;c;%s\007', b64)
+
+  -- Try multiple methods to send OSC 52
+  if vim.fn.exists('$TMUX') == 1 then
+    -- Wrap in tmux passthrough if inside tmux
+    osc52 = string.format('\027Ptmux;\027%s\027\\', osc52)
+  end
+
+  io.stdout:write(osc52)
+  io.stdout:flush()
+end
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = function(lines)
+      copy_to_osc52(lines)
+    end,
+    ['*'] = function(lines)
+      copy_to_osc52(lines)
+    end,
+  },
+  paste = {
+    ['+'] = function() return {} end,
+    ['*'] = function() return {} end,
+  },
+}
 opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
